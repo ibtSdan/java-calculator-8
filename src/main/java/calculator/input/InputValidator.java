683a89ff8d9d2e306a1b373af6abbd85b7114b2f
ibtSdan <ibtsdan@gmail.com>
2025-10-16 18:08:45 +0900

    feat(Validation): 최소한의 숫자 및 구분자 검증 기능 구현

--- src/main/java/calculator/input/InputValidator.java
+++ src/main/java/calculator/input/InputValidator.java
@@ -1,11 +1,15 @@
 package calculator.input;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InputValidator {
+    List<Character> delimiters = new ArrayList<>(List.of(',', ':'));
+
     public void validate(String input){
         if (input == null || input.isEmpty()){
             throw new IllegalArgumentException("입력값이 비어있습니다.");
         }
-
 
         if (input.startsWith("//")){
             if (!input.contains("\\n")){
@@ -18,12 +22,25 @@ public class InputValidator {
             if (Character.isDigit(delimiter.charAt(0))){
                 throw new IllegalArgumentException("커스텀 구분자는 숫자를 사용할 수 없습니다.");
             }
+            Character newDelimiter = input.charAt(2);
+            delimiters.add(newDelimiter);
+            input = input.substring(input.indexOf("\\n")+2);
         } else {
             char first = input.charAt(0);
             if (!Character.isDigit(first) && first != ',' && first != ':'){
                 throw new IllegalArgumentException("잘못된 입력 형식입니다. 입력은 구분자나 숫자로 시작하거나, 커스텀 구분자 형식이어야 합니다.");
             }
-            // 정상 입력일 때 나머지 검사 (양수, 구분자)
         }
+
+        boolean hasDelimiter = input.chars().anyMatch(c -> delimiters.contains((char) c));
+        if (!hasDelimiter){
+            throw new IllegalArgumentException("입력값에 최소 1개의 구분자가 포함되어야 합니다.");
+        }
+
+        boolean hasNumber = input.chars().anyMatch(Character::isDigit);
+        if (!hasNumber){
+            throw new IllegalArgumentException("입력값에 최소 1개의 숫자가 포함되어야 합니다.");
+        }
+
     }
 }
